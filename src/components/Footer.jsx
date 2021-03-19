52fe9f5f4aac2515d0f08758ba5b5f85caf56ee4
christeen24 <christeen@incubatelabs.com>
2021-03-19 14:55:44 +0530

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer(props) {
   return (
     <div class="container-fluid">
       <div class="bg-dark footer text-white">
-        <p class="text-center p-4 m-0">Footer Content</p>
+        <p class="text-center p-4 m-0">Copyright</p>
       </div>
     </div>
   );
